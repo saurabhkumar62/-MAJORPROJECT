f027d19b3db1d762cecbaba47f372e27a9b602ea
Saurabh kumar <saurabhkumar6203026@gmail.com>
2025-06-09 21:55:38 +0530

    Removed old classroom files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //   require('dotenv').config();
 // }
 
+
 const dotenv = require("dotenv")
 dotenv.config()
 const express = require("express");
@@ -12,6 +13,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,10 +23,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-const MONGO_URL = "mongodb+srv://saurabhkumar6203026:O57EfgepOwgqo77F@cluster0.fwheaek.mongodb.net/majorProject?retryWrites=true&w=majority&appName=Cluster0"
+const dbUrl = process.env.ATLASDB_URL;
 
 
 
@@ -37,7 +36,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -47,8 +46,21 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -61,6 +73,7 @@ const sessionOptions = {
 // app.get("/", (req, res) => {
 //   res.send("Hi, I am root");
 // });
+
 
 app.use(session(sessionOptions));
 app.use(flash());
